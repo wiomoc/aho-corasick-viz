7e31336a8dfc967ca3f845e8206337b31b6e0c7f
Christoph Walcher <christoph-wa@gmx.de>
2019-10-07 19:13:15 +0200

    +Draw Arrow +Fix Algo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const SVG_NS = "http://www.w3.org/2000/svg";
+const RADIUS = 25;
+const HEIGHT = 80;
+const WIDTH = 100;
 
 class Node {
     constructor(char) {
@@ -23,9 +26,6 @@ class Node {
     }
 
     calcLayout() {
-        let height = 80;
-        let width = 100;
-
         let childWidth = 0;
         let childHeight = 0;
         this.childWidths = [];
@@ -40,18 +40,18 @@ class Node {
             childHeight += 80
         }
 
-        this.leftOffset = (Math.max(width, childWidth) - width) / 2;
+        this.leftOffset = (Math.max(WIDTH, childWidth) - WIDTH) / 2;
 
-        return {height: childHeight + height, width: Math.max(width, childWidth)}
+        return {height: childHeight + HEIGHT, width: Math.max(WIDTH, childWidth)}
     }
 
     calcPosition({left, top} = {left: 0, top: 0}) {
-        this.posX = left + this.leftOffset + 50;
-        this.posY = top + 40;
+        this.posX = left + this.leftOffset + WIDTH / 2;
+        this.posY = top + HEIGHT / 2;
         let i = 0;
         let childLeft = left;
         this.nextNodesByChar.forEach(next => {
-            next.calcPosition({left: childLeft, top: this.posY + 80});
+            next.calcPosition({left: childLeft, top: this.posY + HEIGHT});
             childLeft += this.childWidths[i];
             i++;
         });
@@ -61,7 +61,7 @@ class Node {
         let circle = document.createElementNS(SVG_NS, "circle");
         circle.setAttribute('cx', this.posX);
         circle.setAttribute('cy', this.posY);
-        circle.setAttribute('r', 25);
+        circle.setAttribute('r', RADIUS);
         circle.setAttribute('style', 'fill: white; stroke: blue; stroke-width: 3px;');
 
         let text = document.createElementNS(SVG_NS, "text");
@@ -74,41 +74,89 @@ class Node {
 
 
         this.nextNodesByChar.forEach(next => {
-            let line = document.createElementNS(SVG_NS, 'line');
-            line.setAttribute('x1', this.posX);
-            line.setAttribute('y1', this.posY - 4);
-            line.setAttribute('x2', next.posX);
-            line.setAttribute('y2', next.posY - 4);
-
-            line.setAttribute('style', 'stroke:rgb(255,0,0);stroke-width:2');
-            svg.appendChild(line);
+            let line = new Line(this.posX - 4, this.posY, next.posX - 4, next.posY, 'stroke:rgb(255,0,0);stroke-width:2');
+            line.shorten(RADIUS);
+            line.renderTo(svg);
             next.render(svg);
         });
 
         if (this.suffixLink) {
-            let line = document.createElementNS(SVG_NS, 'line');
-            line.setAttribute('x1', this.posX);
-            line.setAttribute('y1', this.posY);
-            line.setAttribute('x2', this.suffixLink.posX);
-            line.setAttribute('y2', this.suffixLink.posY);
-            line.setAttribute('style', 'stroke:rgb(0,255,0);stroke-width:2');
-            svg.appendChild(line);
+            let line = new Line(this.posX, this.posY, this.suffixLink.posX, this.suffixLink.posY, 'stroke:rgb(0,255,0);stroke-width:2');
+            line.shorten(RADIUS);
+            line.renderTo(svg)
         }
 
         this.endSuffixLinks.forEach(next => {
-            let line = document.createElementNS(SVG_NS, 'line');
-            line.setAttribute('x1', this.posX);
-            line.setAttribute('y1', this.posY + 4);
-            line.setAttribute('x2', next.posX);
-            line.setAttribute('y2', next.posY + 4);
-
-            line.setAttribute('style', 'stroke:rgb(0,0,255);stroke-width:2');
-            svg.appendChild(line);
+            let line = new Line(this.posX, this.posY + 4, next.posX, next.posY, 'stroke:rgb(0,0,255);stroke-width:2');
+            line.shorten(RADIUS);
+            line.renderTo(svg)
         });
 
         svg.appendChild(circle);
         svg.appendChild(text);
     }
+}
+
+class Line {
+    constructor(x1, y1, x2, y2, style) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.style = style;
+        this.length = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2));
+    }
+
+    renderTo(svg) {
+        let line = document.createElementNS(SVG_NS, 'line');
+        line.setAttribute('x1', this.x1);
+        line.setAttribute('y1', this.y1);
+        line.setAttribute('x2', this.x2);
+        line.setAttribute('y2', this.y2);
+        line.setAttribute('style', this.style);
+        svg.appendChild(line);
+
+
+        const xBase = (this.x1 - this.x2) / this.length;
+        const yBase = (this.y1 - this.y2) / this.length;
+        let baseArrowX = this.x2 + xBase * 10;
+        let baseArrowY = this.y2 + yBase * 10;
+        let angleX = Math.acos(xBase);
+        let angleY = Math.asin(yBase);
+        let leftArrowX = baseArrowX + Math.cos(angleY + Math.PI / 2) * 5;
+        let leftArrowY = baseArrowY + Math.sin(angleX + Math.PI / 2) * 5;
+
+        let rightArrowX = baseArrowX + Math.cos(angleY + Math.PI / 2) * -5;
+        let rightArrowY = baseArrowY + Math.sin(angleX + Math.PI / 2) * -5;
+
+
+        let arrowLeftLine = document.createElementNS(SVG_NS, 'line');
+        arrowLeftLine.setAttribute('x1', leftArrowX);
+        arrowLeftLine.setAttribute('y1', leftArrowY);
+        arrowLeftLine.setAttribute('x2', this.x2);
+        arrowLeftLine.setAttribute('y2', this.y2);
+        arrowLeftLine.setAttribute('style', this.style);
+
+        svg.appendChild(arrowLeftLine);
+
+        let arrowRightLine = document.createElementNS(SVG_NS, 'line');
+        arrowRightLine.setAttribute('x1', rightArrowX);
+        arrowRightLine.setAttribute('y1', rightArrowY);
+        arrowRightLine.setAttribute('x2', this.x2);
+        arrowRightLine.setAttribute('y2', this.y2);
+        arrowRightLine.setAttribute('style', this.style);
+
+        svg.appendChild(arrowRightLine);
+    }
+
+    shorten(length) {
+        const newLength = this.length - length;
+        this.y2 = this.y1 + ((this.y2 - this.y1) / this.length) * newLength;
+        this.x2 = this.x1 + ((this.x2 - this.x1) / this.length) * newLength;
+
+        this.length = newLength;
+    }
+
 }
 
 class Graph {
@@ -126,24 +174,25 @@ class Graph {
         let next = node.getNextNodeByChar(char);
         if (!next) {
             next = new Node(char);
+            next.depth = pos;
             this.nodes.push(next);
             node.addNextNode(next);
         }
         this.addWordToNode(next, word, pos + 1)
     }
 
-    findSuffixLink(node, depth = 0, upper = this.root) {
+    findSuffixLink(node, upper = this.root) {
         for (let [nextChar, next] of upper.nextNodesByChar) {
             if (nextChar === node.char) {
                 if (next === node) {
                     node.suffixLink = upper;
                 } else {
                     node.nextNodesByChar.forEach((next2) => {
-                        this.findSuffixLink(next2, depth + 1, next)
+                        this.findSuffixLink(next2, next)
                     });
-                    if (depth > node.suffixLinkDepth) {
-                        node.suffixLinkDepth = depth;
+                    if (next.depth > node.suffixLinkDepth) {
                         node.suffixLink = next;
+                        node.suffixLinkDepth = next.depth;
                     }
 
                     if (next.wordEndings.length > 0) {
@@ -154,7 +203,7 @@ class Graph {
         }
 
         node.nextNodesByChar.forEach((next) => {
-            this.findSuffixLink(next, depth + 1)
+            this.findSuffixLink(next)
         })
     }
 
@@ -190,6 +239,7 @@ graph.addWord("bc");
 graph.addWord("bca");
 graph.addWord("c");
 graph.addWord("caa");
+graph.addWord("dca");
 graph.finish();
 
 console.log(graph.root);
